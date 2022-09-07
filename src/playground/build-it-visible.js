237e363c368bbe6b0bfbeaadc1ab97a78d24a881
Barend Koorzen <koorzenb@gmail.com>
2022-09-07 08:53:56 +0200

    Added VisibilityToggle

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,35 @@
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleVisibility = this.toggleVisibility.bind(this);
+    this.state = {
+      'visibility': false
+    };
+  }
+
+  toggleVisibility() {
+    this.setState(prevState => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleVisibility}>
+          {this.state.visibility ? 'Hide details' : 'Show details'}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Hey. These are some details you can now see!</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
