ca8652bc68c6810de16d668b5c116c1b06f738f6
Barend Koorzen <koorzenb@gmail.com>
2022-09-08 14:41:36 +0200

    Setting up Babel with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-// check webpack.js.org
 const path = require('path');
 
 module.exports = {
     entry: './src/app.js',
-    mode: process.env.NODE_ENV || 'development',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
