d7a3ceb1747a34d169d61d2700b38bb1f89f9b89
Barend Koorzen <koorzenb@gmail.com>
2022-09-08 16:43:49 +0200

    ES6 class properties

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,35 +1,29 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleSubmit.bind(this);
-        this.state = {
-            error: undefined
-        };
+  state = {
+    error: undefined
+  };
+  handleAddOption = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
     }
-
-    handleSubmit(e) {
-        e.preventDefault();
-
-        const option = e.target.elements.option.value.trim();
-        const error = this.props.handleAddOption(option);
-
-        this.setState(() => ({error}));
-
-        if (!error) e.target.elements.option.value = '';
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleAddOption}>
-                    {this.state.error && <p>{this.state.error}</p>}
-                    <input type='text' name='option' placeholder='type an option' />
-
-                    <button>Add Option</button>
-                </form>
-            </div>
-        );
-    }
+  };
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
 }
